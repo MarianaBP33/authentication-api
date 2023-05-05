0706e5658067cd7587e79799a0aca092a5f59e53
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-05-04 18:25:38 -0600

    update group id

--- src/main/java/dev/chetopuffet/authenticationapi/AuthenticationApiApplication.java
+++ src/main/java/dev/chetopuffet/authenticationapi/AuthenticationApiApplication.java
@@ -1,4 +1,4 @@
-package dev.ososuna.authenticationapi;
+package dev.chetopuffet.authenticationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
