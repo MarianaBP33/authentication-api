2e2004430a0b8378e49ec697800d84e6b055ac0e
MarianaB <mariana.bp.33@gmail.com>
2023-05-06 16:25:45 -0600

    little change

--- src/main/java/dev/chetopuffet/authenticationapi/repository/ParoRepository.java
+++ src/main/java/dev/chetopuffet/authenticationapi/repository/ParoRepository.java
@@ -1,9 +1,13 @@
 package dev.chetopuffet.authenticationapi.repository;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import dev.chetopuffet.authenticationapi.model.Paro;
 
-public interface ParoRepository {
+public interface ParoRepository extends JpaRepository<UUID,Paro> {
 
     List<Paro> findAllByActiveTrue();
     
